96cc856305b97f83a8eecfaa20b05b2957182142
drkop <satanatyn@yandex.ru>
2023-02-06 18:36:59 +0300

    1

---
+++ Ex1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse (Console.ReadLine());
+string Day = "нет такого дня недели";
+if (number >0 && number < 6) Day = "Рабочий день";
+if (number >= 6 && number < 8) Day = "Выходной день";
+Console.WriteLine(Day);
