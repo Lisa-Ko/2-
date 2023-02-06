c5c22ce25ec403e7820c15baa2073e45640c3d70
drkop <satanatyn@yandex.ru>
2023-02-06 18:46:30 +0300

    2

---
+++ Ex2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 == number2*number2) Console.WriteLine("Первое число является квадратом второго ");
+else if (number2 == number1*number1) Console.WriteLine("Второе число является квадратом первого ");
+else Console.WriteLine("Ни одно число не является квадратом другого");
