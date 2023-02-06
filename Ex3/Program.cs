fb6ecbf495e4538c010753ac40eb08f84fa62297
drkop <satanatyn@yandex.ru>
2023-02-06 18:56:29 +0300

    3

---
+++ Ex3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0) Console.WriteLine("Точка в первой четверти");
+else if (x < 0 && y > 0) Console.WriteLine("Точка во второй четверти");
+else if (x > 0 && y < 0) Console.WriteLine("Точка в третьей четверти");
+else Console.WriteLine("Точка в четвертой четверти");
